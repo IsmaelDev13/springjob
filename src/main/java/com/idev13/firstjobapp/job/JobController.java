e5f642564526ad1d035579223c2d6364647c6650
Ismael Diaz <ismael-diaz8@hotmail.es>
2024-07-09 23:52:24 +0200

    [feat] delete, put mapping

--- src/main/java/com/idev13/firstjobapp/job/JobController.java
+++ src/main/java/com/idev13/firstjobapp/job/JobController.java
@@ -2,13 +2,21 @@ package com.idev13.firstjobapp.job;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
     private JobService jobService;
 
@@ -16,22 +24,42 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
-    public List<Job> findAll() {
-        return jobService.findAll();
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
     }
 
-    @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job) {
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
         jobService.createJob(job);
-        return "Job added successfully";
+
+        return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
     }
 
-    @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
         Job job = jobService.getJobById(id);
         if (job != null)
-            return job;
-        return new Job(1L, "TestJob", "TestJob", "2000", "2000", "Malaga");
+            return new ResponseEntity<>(job, HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id) {
+        boolean deleted = jobService.deleteJobById(id);
+        if (deleted)
+            return new ResponseEntity<>("Job deleted successfully", HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    // @RequestMapping(value = "/{id}",method = RequestMethod.PUT);
+    public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job updatedJob) {
+        boolean updated = jobService.updateJob(id, updatedJob);
+        if (updated)
+            return new ResponseEntity<>("Job updated successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/idev13/firstjobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/idev13/firstjobapp/job/impl/JobServiceImpl.java
@@ -1,6 +1,7 @@
 package com.idev13.firstjobapp.job.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class JobServiceImpl implements JobService {
 
     @Override
     public List<Job> findAll() {
-        return null;
+        return jobs;
     }
 
     @Override
@@ -33,6 +34,36 @@ public class JobServiceImpl implements JobService {
             }
         }
         return null;
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()) {
+            Job job = iterator.next();
+            if (job.getId().equals(id)) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        for (Job job : jobs) {
+            if (job.getId().equals(id)) {
+                job.setTitle(updatedJob.getTitle());
+                job.setDescription(updatedJob.getDescription());
+                job.setMinsalary(updatedJob.getMinSalary());
+                job.setMaxsalary(updatedJob.getMaxsalary());
+                job.setLocation(updatedJob.getLocation());
+
+                return true;
+            }
+        }
+        return false;
+
     }
 
 }
