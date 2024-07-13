0bd5b802451e1735e5db6720fdc8300280fdf378
Ismael Diaz <ismael-diaz8@hotmail.es>
2024-07-13 15:22:21 +0200

    [feat] update reviews

--- src/main/java/com/idev13/firstjobapp/review/ReviewController.java
+++ src/main/java/com/idev13/firstjobapp/review/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,26 @@ public class ReviewController {
 
         else
             return new ResponseEntity<>("Review Not Saved Successfully", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/reviews/{reviewId}")
+    public ResponseEntity<Review> getReview(@PathVariable Long companyId, @PathVariable Long reviewId) {
+
+        return new ResponseEntity<>(reviewService.getReview(companyId, reviewId), HttpStatus.OK);
+    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId, @PathVariable Long reviewId,
+            @RequestBody Review review) {
+
+        boolean isReviewUpdated = reviewService.updateReview(companyId, reviewId, review);
+        if (isReviewUpdated) {
+
+            return new ResponseEntity<>("Review updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Review not updated", HttpStatus.NOT_FOUND);
+
+        }
     }
 
 }
--- src/main/java/com/idev13/firstjobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/idev13/firstjobapp/review/impl/ReviewServiceImpl.java
@@ -2,7 +2,6 @@ package com.idev13.firstjobapp.review.impl;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.idev13.firstjobapp.company.Company;
@@ -34,6 +33,28 @@ public class ReviewServiceImpl implements ReviewService {
         if (company != null) {
             review.setCompany(company);
             reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+
+        return reviews.stream().filter(review -> review.getId().equals(reviewId)).findFirst().orElse(null);
+
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updatedReview) {
+        if (companyService.getCompanyById(companyId) != null) {
+            updatedReview.setCompany(companyService.getCompanyById(companyId));
+            updatedReview.setId(reviewId);
+            reviewRepository.save(updatedReview);
+
             return true;
         } else {
             return false;
